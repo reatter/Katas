12e5e7fd300be485ba43182db73bed50f89bceec
reatter <michael.lannert@gmail.com>
2018-04-22 21:19:03 +0200

    eloquentJS list exercise

---
+++ EloquentJS/Ex/List/list.js
@@ -0,0 +1,11 @@
+// implement a list object which has a list as its next property
+// the last element is null terminated
+// implement listToArray and arrayToList
+
+
+let example =   {   value:1,
+                    rest:{{value:2,
+                           rest:{{value:3,
+                                  rest:{null}}}}}}
+
+console.log(example)
