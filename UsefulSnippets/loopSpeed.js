f3cae45eddc020304f54228a543f2dfb2431df20
reatter <michael.lannert@gmail.com>
2018-10-15 20:39:18 +0200

    tried fugitive.vim and used vim linting on javascript-file

--- UsefulSnippets/loopSpeed.js
+++ UsefulSnippets/loopSpeed.js
@@ -1,22 +1,22 @@
 /*
-from Javascript for cats
+   from Javascript for cats
 http://jsforcats.com/#callbacks
 */
 
 function measureLoopSpeed() {
-    var count = 0
-    function addOne() { count = count + 1 }
+	var count = 0
+		function addOne() { count = count + 1 }
 
-    // Date.now() returns a big number representing the number of
-    // milliseconds that have elapsed since Jan 01 1970
-    var now = Date.now()
+	// Date.now() returns a big number representing the number of
+	// milliseconds that have elapsed since Jan 01 1970
+	var now = Date.now()
 
-    // Loop until Date.now() is 1000 milliseconds (1 second) or more into
-    // the future from when we started looping. On each loop, call addOne
-    while (Date.now() - now < 1000) addOne()
+		// Loop until Date.now() is 1000 milliseconds (1 second) or more into
+		// the future from when we started looping. On each loop, call addOne
+		while (Date.now() - now < 1000) addOne()
 
-    // Finally it has been >= 1000ms, so let's print out our total count
-    console.log(count)
+			// Finally it has been >= 1000ms, so let's print out our total count
+			console.log(count)
 }
 
 measureLoopSpeed()
